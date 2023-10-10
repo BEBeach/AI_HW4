6d76af3626ef12e2f56be38709d9736f96580bfd
Brian Beach <brian@mwc-069246.dhcp.missouri.edu>
2023-10-10 11:33:40 -0500

    Game Update

--- game.py
+++ game.py
@@ -4,9 +4,12 @@ class TicTacToe:
     def __init__(self):
         self.board = [[' ' for _ in range(6)] for _ in range(5)]
         self.moves = []
+        self.finished = False
+        self.winner = ''
     
     def is_valid_start(self, x, y):
-        return 0 <= x < 6 and 0 <= y < 5 and self.board[y][x] == ' '
+        if not self.finished:
+            return 0 <= x < 6 and 0 <= y < 5 and self.board[y][x] == ' '
 
     # Start Function, places the initial pieces
     def start(self, x, y, player):
@@ -19,7 +22,7 @@ class TicTacToe:
     # Checks if next move is valid
     def is_valid_successor(self, x, y):
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-        if self.board[y][x] != ' ':
+        if self.board[y][x] != ' ' or self.finished:
             return False
         for dx, dy in directions:
             if 0 <= x + dx < 6 and 0 <= y + dy < 5 and self.board[y + dy][x + dx] != ' ':
@@ -31,6 +34,7 @@ class TicTacToe:
         if self.is_valid_successor(x, y):
             self.board[y][x] = player
             self.moves.append((player, 'Move', (x, y)))
+            self.end_condition()
         else:
             raise ValueError("Invalid move")
     
@@ -79,14 +83,20 @@ class TicTacToe:
     def end_condition(self):
         if self.has_won('X'):
             game.save_moves('moves.csv')
-            return 'X Wins!'
+            game.finished = True
+            game.winner = 'X'
+            print("X Wins!")
         if self.has_won('O'):
             game.save_moves('moves.csv')
-            return 'O Wins!'
+            game.finished = True
+            game.winner = 'O'
+            print("O Wins!")
         if self.is_full():
             game.save_moves('moves.csv')
-            return 'Tie'
-        return None
+            game.finished = True
+            game.winner = 'Tie'
+            print("Tie!")
+        return False
 
     # Displays the games state in the console
     def display(self):
@@ -108,39 +118,24 @@ class TicTacToe:
 game = TicTacToe()
 
 # Initial moves
-game.start(2, 2, 'X')  # Player X places at (2, 2)
-game.display()
+game.start(2, 2, 'X')
 
-game.start(3, 2, 'O')  # Player O places at (3, 2)
-game.display()
+game.start(3, 2, 'O')
 
 # Players take turns to make their moves
-game.successor(3, 1, 'X')  # Player X places next to their existing piece
-game.display()
+game.successor(3, 1, 'X')
 
-game.successor(4, 2, 'O')  # Player O places next to their existing piece
-game.display()
+game.successor(4, 2, 'O')
 
-game.successor(3, 3, 'X')  # Player X makes another move
-game.display()
+game.successor(3, 3, 'X') 
 
-game.successor(4, 3, 'O')  # Player O makes another move
-game.display()
+game.successor(4, 3, 'O')
 
-game.successor(2, 1, 'X')  # Player X's move
-game.display()
+game.successor(2, 1, 'X')
 
-game.successor(4, 1, 'O')  # Player O's move, now has 4 in a column vertically
-game.display()
+game.successor(4, 1, 'O')
 
-game.successor(2, 0, 'X')  # Player X's move
-game.display()
+game.successor(2, 0, 'X')
 
-game.successor(4, 0, 'O')  # Player O's move, now has 4 in a column vertically
+game.successor(4, 0, 'O') 
 game.display()
-
-# Check for end condition
-result = game.end_condition()
-if result:
-    print(f"Game over. Result: {result}")
-    game.save_moves('moves.csv')
