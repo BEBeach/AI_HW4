fad7ffa441462d89713d05271aa99661be5a1de0
Brian Beach <brian@mwc-068164.dhcp.missouri.edu>
2023-10-12 22:04:35 -0500

    HW4 final draft

--- HW4.py
+++ HW4.py
@@ -1,4 +1,5 @@
 import csv
+import time
 import math
 
 class TicTacToe:
@@ -7,6 +8,7 @@ class TicTacToe:
         self.moves = []
         self.finished = False
         self.winner = ''
+        self.metrics = []
 
     def is_valid_start(self, x, y):
         return not self.finished and 0 <= x < 6 and 0 <= y < 5 and self.board[y][x] == ' '
@@ -27,10 +29,10 @@ class TicTacToe:
                 return True
         return False
 
-    def successor(self, x, y, player):
+    def successor(self, x, y, player, time, nodes):
         if self.is_valid_successor(x, y, player):
             self.board[y][x] = player
-            self.moves.append((player, 'Move', (x, y)))
+            self.moves.append((player, 'Move', (x, y), time, nodes))
             self.end_condition()
         else:
             print(f"Player {player}: {x}, {y}")
@@ -59,7 +61,7 @@ class TicTacToe:
     def save_moves(self, filename):
         with open(filename, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(["Player", "Turn", "Move"])
+            writer.writerow(["Player", "Turn", "Move", "Time", "Nodes Generated"])
             writer.writerows(self.moves)
 
     def end_condition(self):
@@ -90,71 +92,95 @@ class TicTacToe:
             print("|")
         print("  " + "-"*19)
 
-    def minimax(self, depth, max_player, alpha, beta):
+    def minimax(self, depth, max_player, alpha, beta, player, counter=None):
+        if counter is None:
+            counter = [0]
+        counter[0] += 1
+        
         if depth == 0 or self.has_won('X') or self.has_won('O') or self.is_full():
-            return 1000 if self.has_won('X') else (-1000 if self.has_won('O') else 0)
+            if self.has_won(player):
+                return 1000, counter
+            elif self.has_won(opp_player(player)):
+                return -1000, counter
+            else:
+                return 0, counter
+            
         if max_player:
             max_eval = -math.inf
             for col in range(6):
                 for row in range(5):
                     if self.board[row][col] == ' ':
                         self.board[row][col] = 'X'
-                        eval = self.minimax(depth - 1, False, alpha, beta)
+                        eval, _ = self.minimax(depth - 1, False, alpha, beta, player)
                         self.board[row][col] = ' '
                         max_eval = max(max_eval, eval)
                         alpha = max(alpha, eval)
                         if beta <= alpha:
                             break
-            return max_eval
+            return max_eval, counter
         else:
             min_eval = math.inf
             for col in range(6):
                 for row in range(5):
                     if self.board[row][col] == ' ':
                         self.board[row][col] = 'O'
-                        eval = self.minimax(depth - 1, True, alpha, beta)
+                        eval, _ = self.minimax(depth - 1, True, alpha, beta, player)
                         self.board[row][col] = ' '
                         min_eval = min(min_eval, eval)
                         beta = min(beta, eval)
                         if beta <= alpha:
                             break
-            return min_eval
+            return min_eval, counter
 
     def find_best_move(self, player, depth):
-        best_move = None
+        best_row = None
+        best_col = None
         best_eval = -math.inf if player == 'X' else math.inf
+        nodes = 0
         for col in range(6):
             for row in range(5):
                 if self.board[row][col] == ' ' and self.is_valid_successor(col, row, player):
-                    print(col, row)
                     self.board[row][col] = player
-                    eval = self.minimax(depth, player != 'X', -math.inf, math.inf)
+                    eval, gen_nodes = self.minimax(depth, player != 'X', -math.inf, math.inf, player)
+                    nodes += gen_nodes[0]
                     self.board[row][col] = ' '
                     is_better_move = (eval > best_eval) if player == 'X' else (eval < best_eval)
                     if is_better_move:
                         best_eval = eval
-                        best_move = (row, col)
-        return best_move
+                        best_row = row
+                        best_col = col
+        return best_row, best_col, nodes
 
+def opp_player(player):
+    if player == 'X':
+        return 'O'
+    else:
+        return 'X'
+    
 def play_tic_tac_toe():
     game = TicTacToe()
     current_player = True  # True -> X, False -> O
-    game.start(3, 4, 'X')
+    game.start(4, 3, 'X')
     game.start(3, 3, 'O')
     while not game.finished:
         game.display()
         if current_player:
-            y, x = game.find_best_move('X', 2)
-            if (x, y) is None:
+            start_time = time.time()
+            y, x, gen_nodes = game.find_best_move('X', 2)
+            stop_time = time.time()
+            print(x, y)
+            if x is None:
                 print("No valid move found! Exiting game.")
                 return
-            game.successor(x, y, 'X')
+            game.successor(x, y, 'X', stop_time - start_time, gen_nodes)
         else:  # Computer's turn
-            y, x = game.find_best_move('O', 4) 
-            if (x, y) is None:
+            start_time = time.time()
+            y, x, gen_nodes = game.find_best_move('O', 4) 
+            stop_time = time.time()
+            if x is None:
                 print("No valid move found! Exiting game.")
                 return
-            game.successor(x, y, 'O')
+            game.successor(x, y, 'O', stop_time - start_time, gen_nodes)
         current_player = not current_player
         game.display()
 
